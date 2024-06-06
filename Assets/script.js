6a6e836164a79d3d59751f0592fd8341508d9ad7
Matt <mkeeney4202@gmail.com>
2024-06-05 21:33:27 -0400

    fetched movie data

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,26 @@
-const apiKey = "651c5a7f"
+const apiKey = "d254f211"
 
 const searchButton = document.querySelector(".btn")
 
+function searchMovie(movie){
+fetch(
+
+`http://www.omdbapi.com/?s=${movie}&apikey=651c5a7f`
+).then(function(response){
+    return response.json();
+}).then(function(data){
+
+    console.log(data)
+    
+})
+
+
+}
+
+
+searchButton.addEventListener("click", function(event){
+event.preventDefault()
+const movie = document.querySelector("#input-box").value
+searchMovie(movie)
+
+})
