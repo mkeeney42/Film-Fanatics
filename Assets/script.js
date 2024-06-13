7e31b58251f27f80231c779d6df6746326dc330f
Matt <mkeeney4202@gmail.com>
2024-06-12 21:33:56 -0400

    git push not working?

--- Assets/script.js
+++ Assets/script.js
@@ -99,7 +99,7 @@ function renderResult(movie) {
     <div class="flex flex-col justify-between h-full">
     <div>
         <h3 class="text-2xl font-bold mb-1 text-white">Title: ${movie.Title}</h3>
-        <p class="mb-1 font-bold text-white">Year: ${movie.Year}</p>
+        <p class="text-lg mb-1 font-bold text-white">Year: ${movie.Year}</p>
     </div>
     <a href="./second_page.html?movieID=${movie.imdbID}">
         <img class="w-full h-auto object-cover" src="${movie.Poster}" alt="${movie.Title} Poster"/>
